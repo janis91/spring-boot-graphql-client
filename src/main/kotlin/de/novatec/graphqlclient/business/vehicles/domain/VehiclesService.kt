e1a68c7e94a8aeaddb9e2a35ad73da4b9a0183ba
Janis Köhr <janis.koehr@novatec-gmbh.de>
2020-01-14 13:54:00 +0100

    Added ValidationException for retrieved null value case.
    Signed-off-by: Janis Köhr <janis.koehr@novatec-gmbh.de>

--- src/main/kotlin/de/novatec/graphqlclient/business/vehicles/domain/VehiclesService.kt
+++ src/main/kotlin/de/novatec/graphqlclient/business/vehicles/domain/VehiclesService.kt
@@ -6,8 +6,8 @@ import de.novatec.graphqlclient.queries.VehiclesQuery
 import mu.KotlinLogging
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.util.*
 import java.util.stream.Collectors
+import javax.validation.ValidationException
 
 private val LOG = KotlinLogging.logger {}
 
@@ -15,7 +15,7 @@ class VehiclesService(private val client: GraphQlClient, private val config: App
     suspend fun retrieveAndTransformVehicles(): List<Vehicle> {
         LOG.debug { "Retrieve all vehicles available for the demo." }
         val retrievedElements =
-            ((client.query(VehiclesQuery())?.nodes?.elements ?: Collections.emptyList<VehiclesQuery.Element>()))
+            ((client.query(VehiclesQuery())?.nodes?.elements ?: throw ValidationException("The retrieved data is null for root, 'nodes' or 'elements' attribute, which is not allowed.")))
                 .requireNoNulls()
         LOG.debug { "Transform retrieved vehicles to domain objects." }
         return retrievedElements.stream().map { element ->
